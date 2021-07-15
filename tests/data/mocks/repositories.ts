36d65150de2870bca8f9fd6a0e31c484190168a0
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-07-14 22:06:40 -0300

    feat: add repository mocks

--- tests/data/mocks/repositories.ts
+++ tests/data/mocks/repositories.ts
@@ -1,6 +1,6 @@
+import { AddEmployeeRepository, DeleteEmployeeRepository, FindEmployeeRepository, UpdateEmployeeRepository } from '@/data/protocols'
 import { EmployeeModel } from '@/domain/models'
 import { AddEmployeeParams } from '@/domain/usecases'
-import { AddEmployeeRepository } from '@/data/protocols'
 import { mockEmployeeModel } from '@/tests/domain/mocks/employee'
 
 export class AddEmployeeRepositorySpy implements AddEmployeeRepository {
@@ -8,6 +8,37 @@ export class AddEmployeeRepositorySpy implements AddEmployeeRepository {
   employeeModel: EmployeeModel = mockEmployeeModel()
 
   async add (params: AddEmployeeParams): Promise<EmployeeModel> {
+    this.params = params
+    return this.employeeModel
+  }
+}
+
+export class DeleteEmployeeRepositorySpy implements DeleteEmployeeRepository {
+  id: string
+  result: boolean = false
+
+  async delete (id: string): Promise<boolean> {
+    this.id = id
+    return this.result
+  }
+}
+
+export class FindEmployeeRepositorySpy implements FindEmployeeRepository {
+  id: string
+  employeeModel: EmployeeModel = mockEmployeeModel()
+
+  async find (id: string): Promise<EmployeeModel> {
+    this.id = id
+    return this.employeeModel
+  }
+}
+export class UpdateEmployeeRepositorySpy implements UpdateEmployeeRepository {
+  params: AddEmployeeParams
+  id: string
+  employeeModel: EmployeeModel = mockEmployeeModel()
+
+  async update (id: string, params: AddEmployeeParams): Promise<EmployeeModel> {
+    this.id = id
     this.params = params
     return this.employeeModel
   }
